0785e6b9c9655cf48e0b8d127f9fd7f4c3b3dd1b
alisawuffles <alisa.46@gmail.com>
2018-12-11 17:32:01 -0600

    uncomment

--- attribute_analysis/main.py
+++ attribute_analysis/main.py
@@ -6,18 +6,18 @@ def main():
     [b, gs, gsp, gspr] = parse.parse()
     W_data, s_data, groups, good_data, good_groups, pos_data = preprocessing.get_data(gspr)
 
-    # with warnings.catch_warnings():
-    #     warnings.simplefilter('ignore')
-    #     training.train_alpha(W_data, groups, 'W')
-    #
-    #     for label in training.good_labels:
-    #         training.train_alpha(good_data, good_groups, label)
-    #
-    #     training.train_s(s_data, groups)
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        training.train_alpha(W_data, groups, 'W')
+
+        for label in training.good_labels:
+            training.train_alpha(good_data, good_groups, label)
+
+        training.train_s(s_data, groups)
 
     analysis.atoms(gspr)
-    # analysis.pos_table(pos_data)
-    # analysis.show_examples(gspr)
+    analysis.pos_table(pos_data)
+    analysis.show_examples(gspr)
 
 
 if __name__ == "__main__":
